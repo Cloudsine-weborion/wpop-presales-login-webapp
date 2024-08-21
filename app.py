d0258eef00e9bcdc3fed06d8dcd3eb3b17ce44ba
gary <gary@cloudsine.tech>
2024-08-21 12:22:08 +0000

    create end point for health check, login and rerouting to demo site

--- app.py
+++ app.py
@@ -1,21 +1,105 @@
-from fastapi import FastAPI, Request
+from typing import Dict, List, Optional
+from fastapi import FastAPI, Request, Response, Depends, HTTPException, status
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.security import OAuth2PasswordRequestForm
+from fastapi.responses import RedirectResponse
+
+from settings import settings
+from model import User
+from auth import authenticate_user, create_access_token, get_current_user_from_token
+
+
+class LoginForm:
+    def __init__(self, request: Request):
+        self.request: Request = request
+        self.errors: List = []
+        self.username: Optional[str] = None
+        self.password: Optional[str] = None
+
+    async def load_data(self):
+        form = await self.request.form()
+        self.username = form.get("username")
+        self.password = form.get("password")
+
+    async def is_valid(self):
+        if not self.username:
+            self.errors.append("Username is required")
+        if not self.password or not len(self.password) >= 4:
+            self.errors.append("A valid password is required")
+        if not self.errors:
+            return True
+        return False
+
 
 app = FastAPI()
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
+# app.mount("/static", StaticFiles(directory="static"), name="static")
 
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/healthz", status_code=200)
+@app.get("/", status_code=200)
 async def root():
     return {"healthy!"}
 
 
-@app.get("/login", response_class=HTMLResponse)
-# async def login_page(request: Request, id: str):
-async def login(request: Request):
-    return templates.TemplateResponse(request=request, name="login.html")
+@app.post("/token")
+def login_for_access_token(
+    response: Response, form_data: OAuth2PasswordRequestForm = Depends()
+) -> Dict[str, str]:
+    # highlight-start
+    user = authenticate_user(form_data.username, form_data.password)
+    # highlight-end
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+        )
+    access_token = create_access_token(data={"username": user.username})
+
+    # Set an HttpOnly cookie in the response. `httponly=True` prevents
+    # JavaScript from reading the cookie.
+    # highlight-start
+    response.set_cookie(
+        key=settings.COOKIE_NAME, value=f"Bearer {access_token}", httponly=True
+    )
+    # highlight-end
+    return {settings.COOKIE_NAME: access_token, "token_type": "bearer"}
+
+
+@app.get("/demo", response_class=RedirectResponse, status_code=302)
+# highlight-start
+def index(request: Request, user: User = Depends(get_current_user_from_token)):
+    # highlight-end
+    context = {"user": user, "request": request}
+    return RedirectResponse("https://typer.tiangolo.com")
+
+
+# --------------------------------------------------------------------------
+# Login - GET
+# --------------------------------------------------------------------------
+@app.get("/auth/login", response_class=HTMLResponse)
+def login_get(request: Request):
+    context = {
+        "request": request,
+    }
+    return templates.TemplateResponse("login.html", context)
+
+
+@app.post("/auth/login", response_class=HTMLResponse)
+async def login_post(request: Request):
+    form = LoginForm(request)
+    await form.load_data()
+    if await form.is_valid():
+        try:
+            response = RedirectResponse("/demo", status.HTTP_302_FOUND)
+            login_for_access_token(response=response, form_data=form)
+            form.__dict__.update(msg="Login Successful!")
+            return response
+        except HTTPException:
+            form.__dict__.update(msg="")
+            form.__dict__.get("errors").append("Incorrect Email or Password")
+            return templates.TemplateResponse("login.html", form.__dict__)
+    return templates.TemplateResponse("login.html", form.__dict__)
