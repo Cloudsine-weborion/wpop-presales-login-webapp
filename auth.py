7f4342551dd43178dac15a4f2e0d655afd491e44
gary <gary@cloudsine.tech>
2024-08-21 12:20:21 +0000

    implement oauth2 to store in cookie, gen jwt token, user authentication and decodecode token

---
+++ auth.py
@@ -0,0 +1,125 @@
+# Lib imports
+from fastapi import Request, HTTPException, status, Depends
+from fastapi.openapi.models import OAuthFlows as OAuthFlowsModel
+from fastapi.security.utils import get_authorization_scheme_param
+from fastapi.security import OAuth2
+from passlib.handlers.sha2_crypt import sha512_crypt as crypto
+import datetime as dt
+from typing import Optional, Dict
+from jose import jwt, JWTError
+
+
+# Functions imports
+from settings import settings
+from model import get_user, User
+
+
+class OAuth2PasswordBearerWithCookie(OAuth2):
+    def __init__(
+        self,
+        tokenUrl: str,
+        scheme_name: Optional[str] = None,
+        scopes: Optional[Dict[str, str]] = None,
+        description: Optional[str] = None,
+        auto_error: bool = True,
+    ):
+        if not scopes:
+            scopes = {}
+        flows = OAuthFlowsModel(password={"tokenUrl": tokenUrl, "scopes": scopes})
+        super().__init__(
+            flows=flows,
+            scheme_name=scheme_name,
+            description=description,
+            auto_error=auto_error,
+        )
+
+    async def __call__(self, request: Request) -> Optional[str]:
+        # highlight-start
+        authorization: str = request.cookies.get(settings.COOKIE_NAME)
+        # highlight-end
+        scheme, param = get_authorization_scheme_param(authorization)
+        if not authorization or scheme.lower() != "bearer":
+            if self.auto_error:
+                raise HTTPException(
+                    status_code=status.HTTP_401_UNAUTHORIZED,
+                    detail="Not authenticated",
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+            else:
+                return None
+        return param
+
+
+oauth2_scheme = OAuth2PasswordBearerWithCookie(tokenUrl="token")
+
+import pdb
+
+
+def create_access_token(data: Dict) -> str:
+    to_encode = data.copy()
+    expire = dt.datetime.now() + dt.timedelta(
+        minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES
+    )
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(
+        to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM
+    )
+    return encoded_jwt
+
+
+def authenticate_user(username: str, plain_password: str) -> User:
+    user = get_user(username)
+    # highlight-start
+    if not user:
+        return False
+    if not crypto.verify(plain_password, user.hashed_password):
+        return False
+    # highlight-end
+    return user
+
+
+def decode_token(token: str) -> User:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+    )
+    # pdb.set_trace()
+    # highlight-start
+    # token = token.removeprefix("Bearer").strip()
+    # highlight-end
+    try:
+        payload = jwt.decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
+        username: str = payload.get("username")
+        if username is None:
+            raise credentials_exception
+    except JWTError as e:
+        print(e)
+        raise credentials_exception
+
+    user = get_user(username)
+    return user
+
+
+def get_current_user_from_token(token: str = Depends(oauth2_scheme)) -> User:
+    """
+    Get the current user from the cookies in a request.
+
+    Use this function when you want to lock down a route so that only
+    authenticated users can see access the route.
+    """
+    user = decode_token(token)
+    return user
+
+
+def get_current_user_from_cookie(request: Request) -> User:
+    """
+    Get the current user from the cookies in a request.
+
+    Use this function from inside other routes to get the current user. Good
+    for views that should work for both logged in, and not logged in users.
+    """
+    token = request.cookies.get(settings.COOKIE_NAME)
+    user = decode_token(token)
+    return user
