8ac3d363d3c0cdfbf07e90a951af62604646732d
gary <gary@cloudsine.tech>
2024-08-23 07:12:04 +0000

    secret key loads from env variable

--- settings.py
+++ settings.py
@@ -1,5 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SECRET = os.getenv("SECRET")
+
+
 class Settings:
-    SECRET_KEY: str = "secret-key"
+    SECRET_KEY: str = SECRET
     ALGORITHM = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES = 30  # in mins
     COOKIE_NAME = "access_token"
